d4e6eecca37ce325cade1666a51bb7b2ea2210b7
Jacob Balin <balinj@oregonstate.edu>
2021-02-02 20:12:38 -0800

    List average program with unit testing.

--- list_average.py
+++ list_average.py
@@ -1,11 +1,52 @@
 def main():
-    arr = [1, 2, 3, 4, 5]
-    total = 0
-    for i in arr:
-        total += i
+    try:
+        print("Unit test 1:")
+        arr = [1, 2, 3, 4, 5]
+        total = 0
+        for i in arr:
+            total += i
 
-    ave = total / len(arr)
-    print("\nAverage: ", ave, "\n")
+        ave = total / len(arr)
+        print("\nAverage: ", ave, "\n")
+    except:
+        print("Unit test 1 failed.")
+
+    try:
+        print("Unit test 2:")
+        arr = [-1, -2, -3, -4, -5]
+        total = 0
+        for i in arr:
+            total += i
+    
+        ave = total / len(arr)
+        print("\nAverage: ", ave, "\n")
+    except:
+        print("Unit test 1 failed.")
+
+    try:
+        print("Unit test 3:")
+        arr = [1, -2, 3, -4, 5]
+        total = 0
+        for i in arr:
+            total += i
+
+        ave = total / len(arr)
+        print("\nAverage: ", ave, "\n")
+    except:
+        print("Unit test 3 failed.")
+
+    try:
+        print("Unit test 4:")
+        arr = []
+        total = 0
+        for i in arr:
+            total += i
+
+        ave = total / len(arr)
+        print("\nAverage: ", ave, "\n")
+    except:
+        print("Unit test 4 failed.")
+
         
 if __name__ == "__main__":
     main()
